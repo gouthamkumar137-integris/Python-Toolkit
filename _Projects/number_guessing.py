3378adceb202017bd89ca74e5cb1231b5fbcf99d
gouthamkumar137-integris <gouthamkumar938@gmail.com>
2026-06-03 00:11:19 +0530

    feat(number guessing): Implemented edge cases and error handling for better user experience.

--- _Projects/number_guessing.py
+++ _Projects/number_guessing.py
@@ -1,12 +1,32 @@
 import random
-
+def play_game():
+    
+    count=0
+    print("Guess number between 1 to 100\n")
+    key=random.randint(1,100)
+    while True:
+        try:
+            number=int(input("Enter your guess (1-100): "))
+            if number<1 or number>100:
+                raise ValueError("Please enter a numbeer between 1 and 100.")
+            count+=1
+            if number==key:
+                print("Correct!\n")
+                break
+            elif key>number:
+                print("hint: key is greater than what you guessed.\n")
+            elif key<number:
+                print("hint: key is lower than what you guessed.\n")
+        except ValueError as ve:
+            print(ve)
+        
+        except Exception as e:
+            print("Unexpected Error Occured: ",e)
+        
+    print("You guessed in ",count ,"attempts.")
 while True:
-    key=random.randint(1,10)
-    try:
-        number=int(input("Enter number (1-10): "))
-        if number==key:
-            print("Good guessing")
-        else:
-            print("Correct number is: ",key)
-    except ValueError:
-        print("Invalid value!!!")
+    play_game()
+    choice=input("Play again? (y/n) ").lower()
+    if choice=='n':
+        break
+    
